b4d6ce1b825aeffc984e062456dd3f6a45af9d05
Kay <karla.bailey@iitelsolutions.com>
2026-07-15 21:51:49 -0400

    Update About.tsx

--- About.tsx
+++ About.tsx
@@ -74,8 +74,8 @@ export default function About() {
       img: "https://raw.githubusercontent.com/Chris7stack/Teli-website/813c0684fe46dfe15412d206d35d17d486ae5d8d/public/images/Augustine-Gallego.jpg"
     },
     { 
-      name: "Dr. Jing Luan", 
-      bio: "Dr. Jing Luan is the system provost emeritus of San Mateo County Community College District and a school board member. A globally recognized expert, his pioneering work in AI and data mining has set new standards across the field.", 
+      name: "Dr. Jing Luán",
+      bio: "Dr. Jing Luán is the system provost emeritus of San Mateo County Community College District and a school board member. A globally recognized expert, his pioneering work in AI and data mining has set new standards across the field.", 
       img: "https://raw.githubusercontent.com/Chris7stack/Teli-website/813c0684fe46dfe15412d206d35d17d486ae5d8d/public/images/Jing-luan.jpg"
     },
     { 
